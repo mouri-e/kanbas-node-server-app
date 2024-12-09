51027c59040907af3c6ec9215a2ef8b4d425d6b4
mouri-e <mouri.e@northeastern.edu>
2024-12-08 22:21:20 -0500

    quizzes project

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -1,9 +1,29 @@
-import Database from "../Database/index.js";
+import model from "./model.js";
+//import Database from "../Database/index.js";
+
+export async function findCoursesForUser(userId) {
+ const enrollments = await model.find({ user: userId }).populate("course");
+ return enrollments.map((enrollment) => enrollment.course);
+}
+export async function findUsersForCourse(courseId) {
+ const enrollments = await model.find({ course: courseId }).populate("user");
+ return enrollments.map((enrollment) => enrollment.user);
+}
+export function enrollUserInCourse(user, course) {
+ return model.create({ user, course });
+}
+export function unenrollUserFromCourse(user, course) {
+ return model.deleteOne({ user, course });
+}
+
 
 export function findAllEnrollments() {
-    const { enrollments } = Database;
-    return enrollments;
+    //const { enrollments } = Database;
+    //return enrollments;
+    return model.find();
 }
+
+/*
 
 export function enrollUserInCourse(userId, courseId) {
     const { enrollments } = Database;
@@ -25,3 +45,4 @@ export function unenrollUserFromCourse(userId, courseId) {
         (enrollment) =>
         (!(enrollment.course == courseId && enrollment.user == userId)));
 }
+        */
